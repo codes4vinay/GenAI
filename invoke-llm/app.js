4d0bbd5d3f6733bc93c19f403436793b64c469cc
Vinay Kumar <vkr7.info@gmail.com>
2026-03-26 19:32:12 +0530

    Structured Output in LLM

--- invoke-llm/app.js
+++ invoke-llm/app.js
@@ -17,11 +17,14 @@ async function main() {
         frequency_penalty: 1,  //(Rare)frequency_penalty is a parameter between -2.0 and 2.0 that can be used to discourage the model from repeating the same tokens or phrases in its output. A higher frequency penalty (e.g., 0.5) will make the model less likely to repeat tokens, while a lower value (e.g., 0) will allow for more repetition. This can help improve the diversity of the generated text.
         presence_penalty: 1,   //(Rare)presence_penalty is a parameter between -2.0 and 2.0 that can be used to discourage the model from generating tokens that have already appeared in the input or output. A higher presence penalty (e.g., 0.5) will make the model less likely to generate tokens that are already present, while a lower value (e.g., 0) will allow for more repetition of existing tokens. This can help encourage the model to introduce new concepts or ideas in its response.
         max_completion_tokens: 500,  //max_completion_tokens limits the number of tokens in the generated response. This can help manage the length of the output and control costs associated with token usage.
+        response_format: {'type': 'json_object'},  //response_format specifies the format in which the model should return its output. In this case, "json_object" indicates that the model should return a structured JSON object, which can be easily parsed and used in applications. This is particularly useful when you want to ensure that the output adheres to a specific structure for further processing.
         model: "llama-3.3-70b-versatile",
         messages: [
             {
                 role: "system",    //system role is used to set the behavior of the assistant
-                content: "You are a VK-ai, a smart review grader assistant. Your task is to analyze given reviews and return sentiment.Classify the review as positive, negative, or neutral. Output must be a single word.",  //personna
+                content: `You are a VK-ai, a smart review grader assistant. Your task is to analyze given reviews and return sentiment.Classify the review as positive, negative, or neutral.
+                You must return the result in valid JSON structure.
+                example: {"sentiment": "Positive"}`,  //personna  &&  // structured output so they developer can easily parse the response and use it in their application without needing to implement complex text processing or natural language understanding logic. By providing a clear and consistent format for the output, it simplifies the integration of the LLM's responses into various applications and workflows. && // we can also use zod schema for structured output.
             },
             {
                 role: "user",
@@ -31,10 +34,11 @@ async function main() {
         ]
     });
 
-    console.log(completion.choices[0].message.content);
+    console.log(JSON.parse(completion.choices[0].message.content));
 }
 
 main();
 
 
-
+//Check out here for structured output: https://console.groq.com/docs/structured-outputs
+//Intructor Library is used to verify the output of the LLM against a predefined schema. It helps ensure that the generated response adheres to a specific structure, making it easier to parse and use in applications. By defining a schema, developers can validate the output and handle any discrepancies or errors effectively. This is particularly useful when working with complex data or when integrating LLM responses into larger systems where consistency and reliability are crucial.
